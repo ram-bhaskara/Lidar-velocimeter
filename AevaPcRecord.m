fa74b1dc876db1d2aebee5e3cc10bf1cce3ca998
ram-bhaskara <ramchanderbhaskara@gmail.com>
2024-01-05 16:59:20 -0600

    point cloud animation based on point velocities

--- AevaPcRecord.m
+++ AevaPcRecord.m
@@ -1,16 +1,36 @@
-function AevaPcRecord(pc_data)
+function AevaPcRecord(pc_data, location)
 
-% To record velocimetry
-% spinning rocket body
+    % To record velocimetry % spinning rocket body
 
+    % Assuming pc_data is a structure array with fields xyz and velocity num_frames = numel(pc_data);
+% Total number of frames
 
-myWriter = VideoWriter('spinningSatellite','MPEG-4');
+        fig = figure;
+set(fig, 'Position', [ 100, 100, 560, 420 ]);
+title('Velocity Color-coded Point Cloud Animation');
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+
+myWriter = VideoWriter(strcat(location, '/', 'spinningSatellite'), 'Motion JPEG AVI');
 myWriter.Quality = 100;
 myWriter.FrameRate = 10;
+% myWriter.Width = 200;
+% myWriter.Height = 200;
 open(myWriter);
 
+min_velocity = -1.5;
+max_velocity = 0.45;
+static_color = [ 1.0, 1.0, 1.0 ];
 
-for INPUT_FRAME = 1:3:175
+% logic for corresponding colorbar
+
+colorMap = [linear_interpolate(static_color, [1,0,0], linspace(1,0,128)'); ...
+    linear_interpolate(static_color, [0 0 1], linspace(0,1,128)')]; 
+colormap(colorMap); 
+colorbar;
+
+for INPUT_FRAME = 1:num_frames
 
     pc_temp = pointCloud(double(pc_data(INPUT_FRAME).xyz'));
     roi = [2 5.2 -2 2 -1 1];
@@ -21,48 +41,52 @@ for INPUT_FRAME = 1:3:175
     myZ = pc_select.Location(:,3); 
     
 
-vel = pc_data(INPUT_FRAME).velocity;
-velocity_channel = vel(i_roi);
-min_velocity = -1.5;
-max_velocity = 0.45;
-static_color = [1.0,1.0,1.0];
-
-colors = repmat(static_color, length(velocity_channel), 1); 
-
-pos_idx = find(velocity_channel > 0);
-pos_mag = clip(velocity_channel(pos_idx)/max_velocity, 0, 1); 
-colors(pos_idx,:) = linear_interpolate(static_color,[1.0,0.0,0.0],pos_mag); 
-
-neg_idx = find(velocity_channel < 0);
-neg_mag = clip(velocity_channel(neg_idx)/min_velocity, 0, 1); 
-colors(neg_idx,:) = linear_interpolate(static_color,[0.0,0.0,1.0],neg_mag); 
-
-colors = uint8(255*colors);
-pc_select.Color = colors;
+    vel = pc_data(INPUT_FRAME).velocity;
+    velocity_channel = vel(i_roi);
+   
+    colors = repmat(static_color, length(velocity_channel), 1); 
     
-    figure
+    pos_idx = find(velocity_channel > 0);
+    pos_mag = clip(velocity_channel(pos_idx)/max_velocity, 0, 1); 
+    colors(pos_idx,:) = linear_interpolate(static_color,[1.0,0.0,0.0],pos_mag); 
+    
+    neg_idx = find(velocity_channel < 0);
+    neg_mag = clip(velocity_channel(neg_idx)/min_velocity, 0, 1); 
+    colors(neg_idx,:) = linear_interpolate(static_color,[0.0,0.0,1.0],neg_mag); 
+
+    colors = uint8(255*colors);
+    pc_select.Color = colors;
+    
     pcPlot = pcshow(pc_select);
-    c = colorbar;
-    custom_cmap = load('custom_dockingColormap.mat');
-    colormap(custom_cmap.custom_dockingCmap);
-    c.Color = [1 1 1]; 
-    c.Label.String = 'velocity (m/s)'; c.Label.FontSize = 12;
-    c.Label.Color = [1 1 1];
-    caxis([min_velocity max_velocity]); 
-    xlabel('X'); ylabel('Y'); zlabel('Z');
-    xlim([roi(1) roi(2)]);
-    ylim([roi(3) roi(4)]);
-    zlim([roi(5) roi(6)]);
-    %set(gcf,'color','w');
-    view([90 0]);
 
+        xlim([roi(1) roi(2)]);
+        ylim([roi(3) roi(4)]);
+        zlim([roi(5) roi(6)]);
+        xlabel('X'); ylabel('Y'); zlabel('Z');
+        axis on;
+        set(gcf, 'color', 'k');
+        view([90 0]);
+        
+        
+        
+        c = colorbar;
+        c.Color = [1 1 1];
+        c.Label.String = 'velocity (m/s)'; 
+        c.Label.FontSize = 12;
+        c.Label.Color = [1 1 1];
+        caxis([min_velocity max_velocity]);
+        
 
-    drawnow;
-    writeVideo(myWriter,getframe(gcf));
-    delete(pcPlot);  
+        drawnow;
 
-end
+        frame_data = getframe(fig);
+      
+        frame_data.cdata = imresize(frame_data.cdata, [420, 560]);
+        
+        writeVideo(myWriter, frame_data);
+        clf; % Clear figure for the next iteration
+        
+    end
 
-close(myWriter);
-
+    close(myWriter);
 end
